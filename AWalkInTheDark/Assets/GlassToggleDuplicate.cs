8f737825b776bb7a7e906f7bc03dfb2a420a05e0
Ron Nofar <rnofar@cogswell.edu>
2017-10-16 13:05:57 -0700

    Extra

---
+++ AWalkInTheDark/Assets/GlassToggleDuplicate.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Manifold
+{
+    public class GlassToggleDuplicate : Duplicate {
+
+        [SerializeField] Transform scaleTran;
+        [SerializeField] Renderer rend;
+        private GlassToggle gToggle;
+
+        void Start() {
+            base.Start();
+            gToggle = dupHand.gameObject.GetComponent<GlassToggle>();
+        }
+
+        public override void Act1()
+        {
+            StartCoroutine(
+                GlassToggle.ScaleObject(
+                    scaleTran, 
+                    gToggle.time, 
+                    gToggle.toOne, 
+                    gToggle.X, 
+                    gToggle.Y, 
+                    gToggle.Z));
+            GlassToggle.ChangeMaterial(rend, gToggle.disabledMat);
+        }
+    }
+}
